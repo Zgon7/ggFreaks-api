22546846995c4cbff831d90d25e4c18dc0f8495a
Zgon7 <karim.mannai7@gmail.com>
2020-12-09 00:48:26 +0100

    last

--- routes/produit.js
+++ routes/produit.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get('/produits', produitController.getProduits);
 router.get('/produit/:id', produitController.getProduit);
+router.get('/produit/souscateg/:name', produitController.getBySousCategName);
 router.put('/produit', produitController.updateProduit);
 router.post('/produit', produitController.createProduit);
 router.delete('/produit/:id', produitController.deleteProduit);
